55d3d6e736abaa572da44d5a85cc5f8c7d72aa50
ronansantos181 <160449144+ronansantos181@users.noreply.github.com>
2024-04-23 14:53:35 -0300

    Update 1-variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,12 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Ronan"
+Profissao = "médico'
 # Declare variáveis de cada tipo numérico
-
+numeo = '12'
+numero= '15235'
 
 # Declare uma variável do tipo booleano
+verdadeiro = 'true'
+falso= 'false'
